1518bce89635d8bff1b01d180888f1eb39d3d809
tomfroembgen <tomfroembgen@t-online.de>
2025-05-08 12:23:06 +0200

    transfer @CodingAllan 's algorithms to their directories

---
+++ src/mfwater/algo_mfmc/__init__.py
@@ -0,0 +1,8 @@
+"""
+Argparser
+=========
+
+This module contains the algorithm for the optimal model selection.
+"""
+
+from .mfmc import multifidelity_monte_carlo, evaluate_estimator
---
+++ src/mfwater/algo_model_selection/__init__.py
@@ -0,0 +1,8 @@
+"""
+Argparser
+=========
+
+This module contains the algorithm for the optimal model selection.
+"""
+
+from .model_selection import select_optimal_models
